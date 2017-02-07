471fe1419e41d0db391efb70d73c4e2aea6ec826
Kelsey Ruckert <klr324@rsc64dot1x-53.ems.psu.edu>
2017-02-07 12:45:15 -0500

    Update metadata

--- SLR_scripts/minimize_residuals.R
+++ SLR_scripts/minimize_residuals.R
@@ -6,11 +6,10 @@
 #  -This function find the sum of the absolute residuals estimated from the model
 #       function in "DEoptim_rahm_model.R". Finding the sum is used with the DEoptim
 #       R function to minimize the residuals and find the best values for the
-#       parameters. The best parameter values are used as initial values for the
-#       bootstrap and MCMC codes as described in Ruckert et al. (GRL 2016).
+#       parameters.
 #
 # THIS CODE IS PROVIDED AS-IS WITH NO WARRANTY (NEITHER EXPLICIT
-# NOT IMPLICIT).  I SHARE THIS CODE IN HOPES THAT IT IS USEFUL,
+# NOR IMPLICIT).  I SHARE THIS CODE IN HOPES THAT IT IS USEFUL,
 # BUT I AM NOT LIABLE FOR THE BEHAVIOR OF THIS CODE IN YOUR OWN
 # APPLICATION.  YOU ARE FREE TO SHARE THIS CODE SO LONG AS THE
 # AUTHOR(S) AND VERSION HISTORY REMAIN INTACT.
@@ -18,11 +17,18 @@
 # To use this function, simply source this file:
 #   source("minimize_residuals.R")
 #
+# INPUTS:
+#   vector of parameter values:
+#       p[1]: alpha; sensitivity of sea-level to temperature changes
+#       p[2]: T_0; temperature when sea-level anomaly is zero
+#       p[3]: H_0; initial sea-level anomaly
+#
 ##################################################################################
 
 min_res = function(p){
-    sum(abs(slr - model(p))) # returns the sum of the absolute values
-                             # from sea-level minus simulated model values
+    # Return the sum of the absolute values
+    # from sea-level minus simulated model values
+    sum(abs(slr - model(p)))                        
 }
 
 ################################## END ###########################################
