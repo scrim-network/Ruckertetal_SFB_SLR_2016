576ea4ab3031fac8feb3ebda02e66aad2307b2d0
Kelsey Ruckert <klr324@rsc64dot1x-53.ems.psu.edu>
2017-02-07 12:55:46 -0500

    Update metadata

--- SLR_scripts/searate_func.R
+++ SLR_scripts/searate_func.R
@@ -4,11 +4,10 @@
 #  - Author: Kelsey Ruckert (klr324@psu.edu)
 #
 #  -This function sources the Rahmstorf(2007) model to find the rate of change in
-#       sea-level per year as described in Ruckert et al. (GRL 2016). For further
-#       description and references, please read the paper.
+#       sea level per year.
 #
 # THIS CODE IS PROVIDED AS-IS WITH NO WARRANTY (NEITHER EXPLICIT
-# NOT IMPLICIT).  I SHARE THIS CODE IN HOPES THAT IT IS USEFUL,
+# NOR IMPLICIT).  I SHARE THIS CODE IN HOPES THAT IT IS USEFUL,
 # BUT I AM NOT LIABLE FOR THE BEHAVIOR OF THIS CODE IN YOUR OWN
 # APPLICATION.  YOU ARE FREE TO SHARE THIS CODE SO LONG AS THE
 # AUTHOR(S) AND VERSION HISTORY REMAIN INTACT.
@@ -16,13 +15,26 @@
 # To use this function, simply source this file:
 #   source("searate_func.R")
 #
+# INPUTS:
+#   vector of parameter values:
+#       parameters[1]: alpha; sensitivity of sea-level to temperature changes
+#       parameters[2]: T_0; temperature when sea-level anomaly is zero
+#
+#   vector of temperatures:
+#
 ###################################################################################
 
 thermal = function(parameters, Temp){ #inputs are parameters and temperature
-    a = parameters[1] # sensitivity of sea-level to temperature changes
-    Ti = parameters[2] # temperature when sea-level is zero
+
+    # Extract parameter values
+    a = parameters[1] # sensitivity of sea level to temperature changes
+    Ti = parameters[2] # temperature when sea-level anomaly is zero
+
+    # Estimate the rate of sea-level change each year, Equation (1)
     therm_rate=a*(Temp-Ti)
-    list(therm_rate) #therm_rate returns the rates of sea-level change each year
+
+    # Return the rates of sea-level change each year.
+    list(therm_rate)
 }
 
 #################################### END #########################################
