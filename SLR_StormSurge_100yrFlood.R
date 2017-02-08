331ad7daef85495b75ec3c9f8a03ce37b91bdf94
Kelsey Ruckert <klr324@rsc64dot1x-53.ems.psu.edu>
2017-02-08 09:54:50 -0500

    Clean and update flood script

--- SLR_StormSurge_100yrFlood.R
+++ SLR_StormSurge_100yrFlood.R
@@ -4,24 +4,33 @@
 # Author and copyright: Kelsey Ruckert
 # Pennsylvania State University
 # klr324@psu.edu
-# Code written Dec. 2015
+# Code written Dec. 2015, updated Nov. 2016
 #
-# THIS CODE IS PROVIDED AS-IS WITH NO WARRANTY (NEITHER EXPLICIT
-# NOR IMPLICIT).  I SHARE THIS CODE IN HOPES THAT IT IS USEFUL,
-# BUT I AM NOT LIABLE FOR THE BEHAVIOR OF THIS CODE IN YOUR OWN
-# APPLICATION.  YOU ARE FREE TO SHARE THIS CODE SO LONG AS THE
-# AUTHOR(S) AND VERSION HISTORY REMAIN INTACT.
+##==============================================================================
+## Copyright 2015 Kelsey Ruckert
+## This file is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This file is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this file.  If not, see <http://www.gnu.org/licenses/>.
+##==============================================================================
 #
-# NOTE: This script takes 5+ hours to run
+# NOTE: This script takes 8+ hours to run
 #------------------------------------------------------------------------
 # Estimates potential future 100-yr flood for the San Francisco Bay area
 # based on the current 100-yr storm surge (San_francisco_tides.R) plus
 # various SLR projections in the year 2100 (Project_global_sealevel.R)
 #
-# The various SLR projections include our mean estimate, the estimate
-# in Heberger et al. 2009, and accounting for the SLR uncertainty in 2100.
-#
-# San Francisco, CA
+# The various SLR projections include our mean estimate, our 90% CI estimate,
+# the estimate in Heberger et al. 2009, and accounting for the SLR uncertainty
+# in 2100.
 #########################################################################
 
 rm(list =ls()) #Clear global environment
@@ -42,14 +51,14 @@ source("Scripts/inv_sf.r")
 load("Workspace/SFB_stormSurge.RData")
 
 # Global mean sea-level
-load("Workspace/SFB_globalMeanSeaLevel.RData")
+load("Workspace/SFB_globalMeanSeaLevel2.RData")
 
 ############################# STORM SURGE ESTIMATION ##################################
 #------------------- Estimate current storm surge -------------------------------------
 storm_surgeL <- 1e5 # desired storm surge level
 q = seq(0,1,length.out= storm_surgeL +1)  # quantile array
 
-# Find closed-form solution of GEV fit
+# Find closed-form solution of GEV fit, Equation (5).
 fit_q_year = qgev(q, year.res.max.fit2@fit$par.ests[1], year.res.max.fit2@fit$par.ests[2],
                   year.res.max.fit2@fit$par.ests[3])
 fit_q_year = fit_q_year[fit_q_year< max(fit_q_year)]
@@ -58,12 +67,12 @@ fit_q_year = fit_q_year[fit_q_year< max(fit_q_year)]
 num = which(q <= 0.99)
 num.max = which.max(num)
 year100prob <- num.max +1
-# check to make sure the year100prob value is the 100-yr value (10^-2)
+# check to make sure the year100prob value is the 100-yr value (10^-2 or 0.01)
 print(1-q[year100prob])
 
 # We know the first value in the GEV fit [107cm] has a 100% probability when we account for any
 # amount of future sea-level rise. For finding the inverse of the survival function any values
-# that due not exist will be returned as an NA. To return a value we wil add 107cm will a 100%
+# that due not exist will be returned as an NA. To return a value we will add 107cm will a 100%
 # probability to the vectors accounting for sea-level rise.
 # sea_levelq <- rep(NA, length(q)+1)
 # sea_levelq[1] <- 0
@@ -76,41 +85,51 @@ print(1-q[year100prob])
 # print(1-q[year100prob.sl])
 
 #------------------- Storm surge plus Heberger et al. 2009 --------------------
-#Estimate the storm surge + SLR in heberger et al. 2009 (1.38 m)
-#heberger09.2100 <- fit_q_year/100 + 1.38
-
 # If the first the value is -inf, set the first value to 107 (the second value)
 # which has a probability of 100%.
 print(1-q[1]); print(fit_q_year[1])
 
+#Estimate the storm surge + SLR in heberger et al. 2009
 # Add 107 cm with a probability of 100% to the vector
 heberger09.2100 <- rep(NA, length(fit_q_year))
 heberger09.2100[1] = fit_q_year[2]/100
-heberger09.2100[2:length(heberger09.2100)] = (fit_q_year[2:length(fit_q_year)]/100) + 1.38
+heberger09.2100[2:length(heberger09.2100)] = (fit_q_year[2:length(fit_q_year)]/100) + a2new[45]
+
+# Uncomment to account for water behind and in dams and reserviors (not done in main analysis)
+#heberger09.2100[2:length(heberger09.2100)] = (fit_q_year[2:length(fit_q_year)]/100) + 1.38
 
 #------------------- Storm surge plus mean sea-level rise ---------------------
 #Estimate the mean storm surge + SLR based on the mean of the SLR uncertainty from MCMC
 # heteroskedastic calibration
-# Mean (expected) return level 
-#mean.stormSLR.2100HET <- (fit_q_year/100) + mean(proj2100DamsRes)
-#mean.stormSLR.2100HET <- apply(slr.storm.DR2100, 1, mean)
-
 # Add 107 cm with a probability of 100% to the vector
 mean.stormSLR.2100HET <- rep(NA, length(fit_q_year))
 mean.stormSLR.2100HET[1] = fit_q_year[2]/100
-mean.stormSLR.2100HET[2:length(mean.stormSLR.2100HET)] = (fit_q_year[2:length(fit_q_year)]/100) + mean(proj2100DamsRes)
+mean.stormSLR.2100HET[2:length(mean.stormSLR.2100HET)] = (fit_q_year[2:length(fit_q_year)]/100) + mean(prob_proj2100/100)
+
+# Uncomment to account for water behind and in dams and reserviors (not done in main analysis)
+#mean.stormSLR.2100HET[2:length(mean.stormSLR.2100HET)] = (fit_q_year[2:length(fit_q_year)]/100) + mean(proj2100DamsRes)
+
+#------------------- Storm surge plus 90% quantile sea-level rise ---------------------
+#Estimate the storm surge + 90% SLR based on the mean of the SLR uncertainty from MCMC
+# heteroskedastic calibration
+
+# Add 107 cm with a probability of 100% to the vector
+quant90.stormSLR.2100HET <- rep(NA, length(fit_q_year))
+quant90.stormSLR.2100HET[1] = fit_q_year[2]/100
+quant90.stormSLR.2100HET[2:length(quant90.stormSLR.2100HET)] = (fit_q_year[2:length(fit_q_year)]/100) + quantile(prob_proj2100/100, 0.90)
+
+# Uncomment to account for water behind and in dams and reserviors (not done in main analysis)
+#quant90.stormSLR.2100HET[2:length(quant90.stormSLR.2100HET)] = (fit_q_year[2:length(fit_q_year)]/100) + quantile(proj2100DamsRes, 0.90)
 
 #------------------- Storm surge plus potential sea-level rises ---------------
 #Estimate all the potential storm surge + SLRs based on MCMC heteroskedastic calibration
-#slr.storm.DR2100 <- mat.or.vec(storm_surgeL,length(proj2100DamsRes))
-#for(i in 1:length(proj2100DamsRes)){
-#  slr.storm.DR2100[,i] <- (fit_q_year/100) + proj2100DamsRes[i]
-#}
-
-slr.storm.DR2100 <- mat.or.vec(length(fit_q_year),length(proj2100DamsRes))
-for(i in 1:length(proj2100DamsRes)){
+slr.storm.DR2100 <- mat.or.vec(length(fit_q_year),length(prob_proj2100))
+for(i in 1:length(prob_proj2100)){
     slr.storm.DR2100[1,i] <- fit_q_year[2]/100
-    slr.storm.DR2100[2:length(fit_q_year), i] <- (fit_q_year[2:length(fit_q_year)]/100) + proj2100DamsRes[i]
+    slr.storm.DR2100[2:length(fit_q_year), i] <- (fit_q_year[2:length(fit_q_year)]/100) + (prob_proj2100[i]/100)
+    
+    # Uncomment to account for water behind and in dams and reserviors (not done in main analysis)
+    #slr.storm.DR2100[2:length(fit_q_year), i] <- (fit_q_year[2:length(fit_q_year)]/100) + proj2100DamsRes[i]
 }
 
 ##################### ESTIMATE NEW PROBABILITY OF OCCURRENCE #####################
@@ -124,14 +143,14 @@ heb.probs <- apply(slr.storm.DR2100, 2, inv.sf, val=heberger09.2100[year100prob]
 # If so then the minimum SLR plus the maximum storm surge should be equal to or larger than
 # the 100-yr Heberger 09 value
 print(heberger09.2100[year100prob])
-print(fit_q_year[100000]/100 + min(proj2100DamsRes))
+print(fit_q_year[100000]/100 + min(prob_proj2100/100))
 
-# If False then NAs will be returned for values with probabilities smaller than 1: 100,000
+# If so all NAs produced have a probability smaller than 1:100,000.
 # Set all NAs to 0 since they are smaller than 1:100,000. This gives us a conservative estimate,
 # but the difference/impact of doing so is minor. This effect was tested in the plotting script.
-# See for example supplementary figure 2.
-write.csv(heb.probs,"Data/NewHebergerProbabilities.csv",na="0")
-heb.probs.table <- read.csv("Data/NewHebergerProbabilities.csv")
+# See for example supplementary figure 3.
+write.csv(heb.probs,"Workspace/NewHebergerProbabilities_norm.csv",na="0")
+heb.probs.table <- read.csv("Workspace/NewHebergerProbabilities_norm.csv")
 new.heb.probs = heb.probs.table[,2]
 
 #Find the new probability for the Heberger 09 value:
@@ -148,20 +167,44 @@ mean.slr.probs <- apply(slr.storm.DR2100, 2, inv.sf, val=mean.stormSLR.2100HET[y
 # If so then the minimum SLR plus the maximum storm surge should be equal to or larger than
 # the 100-yr mean SLR + storm surge value
 print(mean.stormSLR.2100HET[year100prob])
-print(fit_q_year[storm_surgeL]/100 + min(proj2100DamsRes))
+print(fit_q_year[storm_surgeL]/100 + min(prob_proj2100/100))
 
-# If False then NAs will be returned for values with probabilities smaller than 1: 100,000
+# If so all NAs produced have a probability smaller than 1:100,000.
 # Set all NAs to 0 since they are smaller than 1:100,000. This gives us a conservative estimate,
 # but the difference/impact of doing so is minor. This effect was tested in the plotting script.
 # See for example supplementary figure 2.
-write.csv(mean.slr.probs,"Data/NewMeanProbabilities.csv",na="0")
-mean.probs.table <- read.csv("Data/NewMeanProbabilities.csv")
+write.csv(mean.slr.probs,"Workspace/NewMeanProbabilities_norm.csv",na="0")
+mean.probs.table <- read.csv("Workspace/NewMeanProbabilities_norm.csv")
 new.mean.probs = mean.probs.table[,2]
 
 #Find the new probability for the mean value:
 new.meanMSLR.prob = mean(new.mean.probs)
 
 remove(mean.slr.probs)
+#------------------- Estimating new 90th quantile probabilities -----------------------
+# Storm surge plus 90% SLR estimate
+# What is the probability of the 100-yr storm surge associated with 90% SLR
+# when you account for all the potential [states of the world] rises in sea-level by the year 2100?
+quant90.slr.probs <- apply(slr.storm.DR2100, 2, inv.sf, val=quant90.stormSLR.2100HET[year100prob])
+
+# Check if probabilities are estimated for each state of the world
+# If so then the minimum SLR plus the maximum storm surge should be equal to or larger than
+# the 100-yr 90%  SLR + storm surge value
+print(quant90.stormSLR.2100HET[year100prob])
+print(fit_q_year[storm_surgeL]/100 + min(prob_proj2100/100))
+
+# If so all NAs produced have a probability smaller than 1:100,000.
+# Set all NAs to 0 since they are smaller than 1:100,000. This gives us a conservative estimate,
+# but the difference/impact of doing so is minor. This effect was tested in the plotting script.
+# See for example supplementary figure 2.
+write.csv(quant90.slr.probs,"Workspace/Newquant90Probabilities_norm.csv",na="0")
+quant90.probs.table <- read.csv("Workspace/Newquant90Probabilities_norm.csv")
+new.quant90.probs = quant90.probs.table[,2]
+
+#Find the new probability for the mean value:
+new.quant90MSLR.prob = mean(new.quant90.probs)
+
+remove(quant90.slr.probs)
 #------------------- Estimating new current storm surge probabilities ----------
 # Storm surge
 # What is the probability of the 100-yr storm surge associated when you account for all 
@@ -172,14 +215,14 @@ nw.storm.slr.probs <- apply(slr.storm.DR2100, 2, inv.sf, val= fit_q_year[year100
 # If so then the minimum SLR plus the maximum storm surge should be equal to or larger than
 # the 100-yr storm surge value
 print(fit_q_year[year100prob]/100)
-print(fit_q_year[storm_surgeL]/100 + min(proj2100DamsRes))
+print(fit_q_year[storm_surgeL]/100 + min(prob_proj2100/100))
 
 which(nw.storm.slr.probs == 'is.na') # integer(0) when no NAs
 
-# If False then NAs will be returned for values with probabilities smaller than 1: 100,000
+# If so all NAs produced have a probability smaller than 1:100,000.
 # Set all NAs to 0 since they are smaller than 1:100,000. This gives us a conservative estimate.
-write.csv(nw.storm.slr.probs,"Data/NewStormProbabilities.csv") #,na="0")
-storm.probs.table <- read.csv("Data/NewStormProbabilities.csv")
+write.csv(nw.storm.slr.probs,"Workspace/NewStormProbabilities_norm.csv") #,na="0")
+storm.probs.table <- read.csv("Workspace/NewStormProbabilities_norm.csv")
 new.storm.probs = storm.probs.table[,2]
 
 #Find the new probability for the Heberger 09 value:
@@ -189,7 +232,7 @@ remove(nw.storm.slr.probs)
 #------------------- Estimate the 100-yr storm surge accounting for all potential SLR anomalies 
 # Create a range of values from:
 # The smallest storm surge value plus the smallest SLR anomaly
-small.num = fit_q_year[2]/100 + min(proj2100DamsRes)
+small.num = fit_q_year[2]/100 + min(prob_proj2100/100)
 small.num = round(small.num, 1)
 end.num = round(mean.stormSLR.2100HET[year100prob], 1)
 num.range1 = seq(small.num, end.num, length.out=20)
@@ -208,8 +251,8 @@ for(i in 1:length(num.range)){
 }
 
 # Set all NAs to 0 since they are smaller than 1:100,000. This gives us a conservative estimate.
-write.csv(new.probs,"Data/NewUncertaintyProbabilities.csv",na="0")
-uncertainty.probs.table <- read.csv("Data/NewUncertaintyProbabilities.csv")
+write.csv(new.probs,"Workspace/NewUncertaintyProbabilities_norm.csv",na="0")
+uncertainty.probs.table <- read.csv("Workspace/NewUncertaintyProbabilities_norm.csv")
 new.uncertainty.probs <- uncertainty.probs.table[,2:101]
 
 average.uncertainty.probs <- rep(NA, length(num.range))
@@ -228,10 +271,11 @@ print(average.uncertainty.probs[max.returnL+1])
 remove(new.probs)
 ##################### PRINT THE NEW PROBABILITY OF OCCURRENCE #####################
 # Current 100-yr storm surge values:
-round(fit_q_year[year100prob]/100,2) #Current storm surge for San Fran.
-round(mean.stormSLR.2100HET[year100prob],2) #Current storm surge + Mean SLR for San Fran.
-round(heberger09.2100[year100prob],2) #Current storm surge + SLR for San Fran.
-round(num.range[max.returnL],2)  # New storm surge + SLR for San Fran. [Accounting for uncertainty]
+round(fit_q_year[year100prob]/100,2) # Current storm surge for San Fran.
+round(mean.stormSLR.2100HET[year100prob],2) # Current storm surge + Mean SLR for San Fran.
+round(quant90.stormSLR.2100HET[year100prob],2) # Current storm surge + 90% SLR for San Fran.
+round(heberger09.2100[year100prob],2) # Current storm surge + SLR for San Fran.
+round(num.range[max.returnL+1],2)  # New storm surge + SLR for San Fran. [Accounting for uncertainty]
 
 #Factor of increase:
 # From Heberger et al 2009 to new estimate:
@@ -242,13 +286,14 @@ round(new.average.prob, 2)
 # 0.01 to:
 round(new.meanMSLR.prob, 2)
 
+# From 90% slr estimate to new estimate:
+# 0.01 to:
+round(new.quant90MSLR.prob, 2)
+
 # From current storm surge (w/o slr) estimate to new estimate:
 # 0.01 to:
 round(new.ssurge.prob, 2)
 
 # Save the workspace for plotting
-save.image(file = "Workspace/SanFranSLR_StormSurge_Analysis.RData")
-
-################################ PLOTTING ##########################################
-# source("PlotRuckertetal_SanFranStormSurSLR.R")
+save.image(file = "Workspace/SanFranSLR_StormSurge_Analysis_norm.RData")
 ################################## END #############################################
