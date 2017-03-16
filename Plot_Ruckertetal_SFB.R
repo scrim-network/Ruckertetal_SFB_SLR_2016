fe8ad242d36f51da81aac756977ffed4abd1ff7a
Kelsey Ruckert <klr324@rsc64dot1x-53.ems.psu.edu>
2017-03-16 12:19:32 -0400

    Fix fig2, fix typos, update legends plot script

--- Plot_Ruckertetal_SFB.R
+++ Plot_Ruckertetal_SFB.R
@@ -65,8 +65,7 @@ mygreen <- brewer.pal(6,"Greens")
 load("Workspace/SanFranSLR_StormSurge_Analysis_norm.RData")
 
 ############################# Figure #1 ##################################
-# SLR and Tide guage plot 
-# The data is the mean for each month from 1784-2013
+# SLR and Tide gauge plot 
 datasanfrantide <- read.csv("Data/9414290_MeanSeaLevelTrends.csv", na.strings = '-999')
 
 # Calculate the 90% confidence interval for the heteroskedastic method: 
@@ -102,7 +101,7 @@ full_page_height    = 8.75
 single_panel_height = 4
 
 #-------------------------- Save plot ------------------------------
-png(file="Figures/Fig1_2016-12-22b.tif", family="Times", units="in", width=text_column_width, height=single_panel_height, pointsize=11, res=300)
+png(file="Figures/Fig1.tif", family="Times", units="in", width=text_column_width, height=single_panel_height, pointsize=11, res=300)
 par(mfrow=c(1,1), mgp=c(1.5,.5,0), mar=c(3.5,4,1,1))
 
 plot(datasanfrantide$Year[55], datasanfrantide$Monthly_MSL[55], typ="l", 
@@ -131,7 +130,7 @@ dev.off()
 
 ############################# Figure #2 ##################################
 ### Block Maxima plots
-png(file="Figures/Fig2_a.tif", family="Times", units="in", width=text_column_width, height=single_panel_height*2, pointsize=11, res=300)
+png(file="Figures/Fig2.tif", family="Times", units="in", width=text_column_width, height=single_panel_height*2, pointsize=11, res=300)
 par(mfrow=c(2,1), mgp=c(1.5,.5,0),mar=c(4, 3, 1, 2))
 plot(index(year.res.max), coredata(year.res.max)/100, pch=20, type = "b", col = "black",#, xaxs = 'i',
 ylab = "Annual block maxima (m)", xlab = "Year", lwd=1.5)
@@ -157,31 +156,33 @@ return_level_plot <- function(block_maxima, max_return_period, legend)
     a1[1] <- a[1] + eps; a2[2] <- a[2] + eps; a3[3] <- a[3] + eps
     f <- c(seq(0.01, 0.09, by = 0.01), 0.1, 0.2, 0.3, 0.4, 0.5,
     0.6, 0.7, 0.8, 0.9, 0.95, 0.99, 0.995, 0.999, 0.9999, 0.99999, 0.999999)
-    q <- gevq(a, 1 - f[which((ceiling(-1/log(f)) - max_return_period <= 0))])
+    q <- gevq(a, 1 - f[which((1/(1-f)) <= (max_return_period+0.01))])
     d <- t(gev.rl.gradient(a = a, p = 1 - f))
     v <- apply(d, 1, q.form, m = mat)
-    plot(-1/log(f[1:length(q)]), q, log = "x", type = "n", xlim = c(0.1, 10^3),
-    ylim = c(min(dat, q), 1.7),#max(dat, q + 1.96 * sqrt(v[1:length(q)]))),
-    xaxt = 'n', cex=1, xlab = "Return period (years)", ylab = "Return level (m)")
+    plot(1/(1-(f[1:length(q)])), q, log = "x", type = "n", xlim = c(0.8, 10^3),
+         ylim = c(min(dat, q), 1.7), #c(min(dat, q), max(dat, q + 1.96 * sqrt(v[1:length(q)]))), 
+         xaxt = 'n', cex=1,
+         xlab = "Return period (years)", 
+         ylab = "Return level (m)")
     axis(1, lwd = 1, at=10^(seq(-1,log10(10^3), by = 1)), label=c(0.1, 1, 10, 100, 1000))
     axis(2, lwd = 1)
-    lines(-1/log(f[1:length(q)]), q, lty = 1, lwd = 2)
-    #lines(-1/log(f[1:length(q)]), q + 1.96 * sqrt(v[1:length(q)]), col = "skyblue", lwd = 1.5)
-    #lines(-1/log(f[1:length(q)]), q - 1.96 * sqrt(v[1:length(q)]), col = "skyblue", lwd = 1.5)
-    points(-1/log((1:length(dat))/(length(dat) + 1)), sort(dat), lwd = 1, cex = 0.75, pch = 21, bg = "white")
+    lines(1/(1-(f[1:length(q)])), q, lty = 1, lwd = 2)
+    # lines(1/(1-(f[1:length(q)])), q + 1.96 * sqrt(v[1:length(q)]), col = "#0080FFFF", lwd = 1.5)
+    # lines(1/(1-(f[1:length(q)])), q - 1.96 * sqrt(v[1:length(q)]), col = "#0080FFFF", lwd = 1.5)
+    points(1/(1-(1:length(dat)/(length(dat) + 1))), sort(dat), lwd = 1, cex = 0.75, pch = 21, bg = "white")
     box(lwd = 1)
     
     if(legend == TRUE | legend == T)
     {
-        legend("topleft",
-        c("Annual block maxima observations", "Best estimate"),
-        col = c("black", "black"),
-        pt.bg = c("white", NA),
-        pch = c(21, NA),
-        lty = c(NA, 1), cex=1,
-        lwd = c(1.5, 1.5),
-        bty = 'n',
-        inset = c(0.01, -0.01))
+      legend("topleft",
+             c("Annual block maxima observations", "Best estimate"),
+             col = c("black", "black"),
+             pt.bg = c("white", NA),
+             pch = c(21, NA),
+             lty = c(NA, 1), cex=1,
+             lwd = c(1.5, 1.5),
+             bty = 'n',
+             inset = c(0.01, -0.01))
     }
     
 }
@@ -192,11 +193,10 @@ put.fig.letter("b.",font=2, cex=1)
 dev.off()
 
 ############################# Figure #3 ##################################
-#Plot pdf of sea-level plus accounting for runoff changes versus 
-# the current SLR accounting for runoff in Heberger 09
+#Plot pdf of sea-level
 
 #-- Probability density plot ------------------------
-png(file="Figures/Fig3_norm_a.tif", family="Times", units="in", width=text_column_width, height=single_panel_height*2, pointsize=11, res=300)
+png(file="Figures/Fig3.tif", family="Times", units="in", width=text_column_width, height=single_panel_height*2, pointsize=11, res=300)
 par(mfrow=c(2,1), mgp=c(1.5,.5,0),mar=c(4, 3, 1, 2.5))
 
 #pdf2100DamsRes <- density(proj2100DamsRes)
@@ -252,16 +252,15 @@ points(heberger09.2100[year100prob], 1-q[year100prob], pch=21, bg=myheatcolors[7
 points(heberger09.2100[year100prob], new.average.prob, pch=21, bg=myheatcolors[7])
 points(num.range[max.returnL+1], average.uncertainty.probs[max.returnL+1], pch=21, bg=myheatcolors[9])
 
-legend("topright",
-c("Observations", "Current storm surge + potential SLR", "Current storm surge",
-"Storm surge + mean SLR", "Storm surge + Heberger
-et al. (2009) SLR", "Storm surge + SLR uncertainty"),
-col = c("black", "snow2", myheatcolors[3], myheatcolors[5], myheatcolors[7], myheatcolors[9]),
-pch = c(21, NA, 21, 21, 21, 21),
-pt.bg = c("black", NA, myheatcolors[3], myheatcolors[5], myheatcolors[7], myheatcolors[9]),
-lty = c(NA, 1, 1, 1, 1, 1), y.intersp=c(0.9,0.9,0.9,0.9,1,1.1),
-lwd = c(NA, 1.5, 1.5, 1.5, 1.5, 1.5),
-cex=0.8)
+legend("topright", c("Observations", "Baseline storm surge +\npotential SLR", "Baseline storm surge",
+                     "Flood height accounting\nfor mean SLR", "Flood height accounting\nfor Heberger et al. (2009) SLR", 
+                     "Flood height accounting\nfor SLR uncertainty"),
+       col = c("black", "snow2", myheatcolors[3], myheatcolors[5], myheatcolors[7], myheatcolors[9]),
+       pch = c(21, NA, 21, 21, 21, 21),
+       pt.bg = c("black", NA, myheatcolors[3], myheatcolors[5], myheatcolors[7], myheatcolors[9]),
+       lty = c(NA, 1, 1, 1, 1, 1), y.intersp=c(0.9,0.9,0.9,0.9,1,1.1),
+       lwd = c(NA, 1.5, 1.5, 1.5, 1.5, 1.5),
+       cex=0.8)
 
 put.fig.letter("b.",font=2, cex=1)
 
@@ -275,7 +274,7 @@ dev.off()
 
 # Supp. Figure 1 is a photograph.
 
-############################# Supp. Figure #2 ##################################
+############################# Supp. Figure #3 ##################################
 #----------------------- Code Checking Plots  ----------------------------------
 # This plot displays why the probability occuring increases when accounting for uncertainty
 # General the sea-level rise with +/- 1 sigma
@@ -318,12 +317,12 @@ prob.mil.minussig = inv.sf(Msig1.ss.slr.mil, mean.stormSLR.2100HET[year100prob])
 real.average.sig.prob = mean(c(new.prob.plussig, 1-q[year100prob], prob.mil.minussig))
 #-------------------------------------------
 
-png(file="Figures/S2_Fig_norm_a.tif", family="Times", units="in", width=text_column_width, height=full_page_height, pointsize=13, res=300)
+png(file="Figures/S3_Fig.tif", family="Times", units="in", width=text_column_width, height=full_page_height, pointsize=13, res=300)
 par(mfrow=c(3,1), mgp=c(1.5,.5,0),mar=c(4, 3, 1, 2.5))
 
 plot(pdf2100, col="black", main="", lwd=1.5, xlab="Sea-level anomaly in 2100 (m)",
 sub="With respect to the year 2000",ylim=c(0, 2), xlim=c(-0.1, 3.1), ylab="Probability density",
-     yaxt="n") # SLR (MCMC heteroskedastic calib.) accounting for runoff changes
+     yaxt="n") # SLR (MCMC heteroskedastic calib.)
 
 lines(c(mean(prob_proj2100/100),mean(prob_proj2100/100)), c(-1,1.73), col=mygreen[4], lty=3,lwd=1.5)
 lines(c(minus1sig,minus1sig), c(-1,1.28), col=mygreen[2], lty=3,lwd=1.5)
@@ -370,46 +369,36 @@ text(4, 0.014, "100-yr return period", cex=0.85)
 points(mean.stormSLR.2100HET[year100prob], new.prob.plussig, pch=21, bg=mygreen[6])
 points(mean.stormSLR.2100HET[year100prob], prob.mil.minussig, pch=21, bg=mygreen[2])
 points(mean.stormSLR.2100HET[year100prob], 1-q[year100prob], pch=21, bg=mygreen[4])
-points(mean.stormSLR.2100HET[year100prob], real.average.sig.prob , pch=8, col="black")
-points(mean.stormSLR.2100HET[year100prob], average.sig.prob , pch=8, col="blue")
+points(mean.stormSLR.2100HET[year100prob], real.average.sig.prob , pch=15, col="black", cex=1.5)
+points(mean.stormSLR.2100HET[year100prob], average.sig.prob , pch=8, col="pink")
 put.fig.letter("b.",font=2, cex=1)
 
-
 legend("bottomright", ncol=1,
-c("Observations", "Current storm surge", "-1 standard deviation",
-"Storm surge + mean SLR", "+1 standard deviation", "Averaged 100-yr return period", "Approximated 100-yr return period"),
-col = c("black", "skyblue", mygreen[2], mygreen[4], mygreen[6], "black", "blue"),
-pch = c(21, NA, 21, 21, 21, 8, 8),
-pt.bg = c("black", NA, mygreen[2], mygreen[4], mygreen[6], NA, NA),
-lty = c(NA, 1, 1, 1, 1, NA, NA),
-lwd = c(NA, 1.5, 1.5, 1.5, 1.5, NA, NA),
-bty = 'n' , cex=0.85)
+       c("Observations", "Baseline storm surge", "Baseline storm surge +\nmean - 1 standard deviation SLR",
+         "Baseline storm surge + mean SLR", "Baseline storm surge +\nmean + 1 standard deviation SLR", 
+         "Averaged 100-yr return period", "Approximated 100-yr return period"),
+       col = c("black", "skyblue", mygreen[2], mygreen[4], mygreen[6], "black", "pink"),
+       pch = c(21, NA, 21, 21, 21, 15, 8),
+       pt.bg = c("black", NA, mygreen[2], mygreen[4], mygreen[6], NA, NA),
+       lty = c(NA, 1, 1, 1, 1, NA, NA),
+       #lwd = c(NA, 1.5, 1.5, 1.5, 1.5, NA, NA),
+       y.intersp=c(0.9,1,1,1,1,1,0.9),
+       lwd = 1.5,
+       bty = 'n' , cex=0.85)
 
 par(mgp=c(1.5,.5,0),mar=c(4, 3, 1, 2.5))
 return.period = c(round(1-q[year100prob],2), round(average.sig.prob,6), round(real.average.sig.prob,6))
 barplot(return.period, xlab="100-yr return period", ylab="Return period at 2.2m (decimals)",
-names.arg=c("Mean 100-yr return period", "Approximated", "Averaged 100-yr return period"), col=c(mygreen[4], "blue", "black"))
+        names.arg=c("Mean 100-yr return period", "Approximated", "Averaged 100-yr return period"), col=c(mygreen[4], "pink", "black"))
 put.fig.letter("c.",font=2, cex=1)
 
 dev.off()
 
-############################# Supp. Figure #3 ##################################
-
-# Supp. Figure 3 is created in ArcGIS ArcMap. Refer to the tutorial on how this is done.
-
-############################# Supp. Figure #4 ##################################
-
-# Supp. Figure 4 is created in ArcGIS ArcMap. Refer to the tutorial on how this is done.
-
-############################# Supp. Figure #5 ##################################
-
-# Supp. Figure 5 is created in ArcGIS ArcMap. Refer to the tutorial on how this is done.
-
-############################# Supp. Figure #6 ##################################
+############################# Supp. Figure #2 ##################################
 # Add the 90% quantile to the figure #3
 
 #-- Probability density plot ------------------------
-png(file="Figures/S6_Fig_norm_a.tif", family="Times", units="in", width=text_column_width, height=single_panel_height*2, pointsize=11, res=300)
+png(file="Figures/S2_Fig.tif", family="Times", units="in", width=text_column_width, height=single_panel_height*2, pointsize=11, res=300)
 par(mfrow=c(2,1), mgp=c(1.5,.5,0),mar=c(4, 3, 1, 2.5))
 
 #pdf2100DamsRes <- density(proj2100DamsRes)
@@ -472,16 +461,15 @@ points(heberger09.2100[year100prob], 1-q[year100prob], pch=21, bg=myheatcolors[7
 points(heberger09.2100[year100prob], new.average.prob, pch=21, bg=myheatcolors[7])
 points(num.range[max.returnL+1], average.uncertainty.probs[max.returnL+1], pch=21, bg=myheatcolors[9])
 
-legend("topright",
-c("Observations", "Current storm surge + potential SLR", "Current storm surge",
-"Storm surge + mean SLR", "Storm surge + Heberger\net al. (2009) SLR", "Storm surge + 90% quantile SLR",
-"Storm surge + SLR uncertainty"),
-col = c("black", "snow2", myheatcolors[3], myheatcolors[5], myheatcolors[7], myheatcolors[8], myheatcolors[9]),
-pch = c(21, NA, 21, 21, 21, 21, 21),
-pt.bg = c("black", NA, myheatcolors[3], myheatcolors[5], myheatcolors[7], myheatcolors[8], myheatcolors[9]),
-lty = c(NA, 1, 1, 1, 1, 1, 1), #y.intersp=c(0.9,0.9,0.9,0.9,1,1,1.1),
-lwd = c(NA, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5),
-cex=0.8)
+legend("topright", c("Observations", "Baseline storm surge +\npotential SLR", "Baseline storm surge",
+                     "Flood height accounting\nfor mean SLR", "Flood height accounting\nfor Heberger et al. (2009) SLR", 
+                     "Flood height accounting\nfor 90% quantile SLR", "Flood height accounting\nfor SLR uncertainty"),
+       col = c("black", "snow2", myheatcolors[3], myheatcolors[5], myheatcolors[7], myheatcolors[8], myheatcolors[9]),
+       pch = c(21, NA, 21, 21, 21, 21, 21),
+       pt.bg = c("black", NA, myheatcolors[3], myheatcolors[5], myheatcolors[7], myheatcolors[8], myheatcolors[9]),
+       lty = c(NA, 1, 1, 1, 1, 1, 1), y.intersp=c(0.9,0.9,0.9,0.9,1,1.1,1.2),
+       lwd = c(NA, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5),
+       cex=0.8)
 
 put.fig.letter("b.",font=2, cex=1)
 
